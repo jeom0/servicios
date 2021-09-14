e12ebb462430979c10e4954d21690ab6a82a0754
jeom0 <jeom0@github.com>
2021-09-14 01:38:52 -0500

    cambio en settings.py

--- servicol/settings.py
+++ servicol/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-albnwx*ph#ian3f!+hf_@(2svoz64-8nm!7=2w@8^x9cik$u-e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['jeom.pythonanywhere.com']
 
 
 # Application definition
